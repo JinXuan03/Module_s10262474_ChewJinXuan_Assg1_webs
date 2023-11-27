2d9fcbebdf678521b782c4d4dec2864b59f5656e
JinXuan03 <s10262474@connect.np.edu.sg>
2023-11-27 22:07:04 +0800

    currently have finish all the page and left only the booking form

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let header = document.querySelector('header');
+let menu = document.querySelector('#image-container');
+let navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
     header.classList.toggle('active', window.scrollY > 0);
 });
+
+menu.onclick = () => {
+  navbar.classList.toggle('active')
+}
+window.onscroll = () => {
+  navbar.classList.remove('active')
+}
 
 function submitForm() {
     // Simple validation
